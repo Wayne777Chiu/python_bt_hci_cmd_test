442afd8991ae75ce64a78499099911ffee7c9711
wayne.chiu <wayne.chiu@wewisetek.com>
2018-05-16 13:27:58 +0800

    update the script for nrw parameter -d

--- source/execute_script.bat
+++ source/execute_script.bat
@@ -8,22 +8,22 @@ bt_hci_cmd_test_wc.py --raw 1 4 fe 3 3 1 0 --time 30
 
 rem this for initialize for normal
 rem HCI_Read_Local_Supported_Features
-bt_hci_cmd_test_wc.py --raw 1 3 10 0
+bt_hci_cmd_test_wc.py -d --raw 1 3 10 0
 rem HCI_LE_Set_Event_Mask
-bt_hci_cmd_test_wc.py --raw 1 1 20 8 1f 0 0 0 0 0 0 0
+bt_hci_cmd_test_wc.py -d --raw 1 1 20 8 1f 0 0 0 0 0 0 0
 rem HCI_LE_Read_Buffer_Size
-bt_hci_cmd_test_wc.py --raw 1 2 20 0
+bt_hci_cmd_test_wc.py -d --raw 1 2 20 0
 rem HCI_Read_Buffer_Size
-bt_hci_cmd_test_wc.py --raw 1 5 10 0
+bt_hci_cmd_test_wc.py -d --raw 1 5 10 0
 rem HCI_LE_Read_Local_Supported_Features
-bt_hci_cmd_test_wc.py --raw 1 3 20 0
+bt_hci_cmd_test_wc.py -d --raw 1 3 20 0 
 rem HCI_Read_BD_ADDR
-bt_hci_cmd_test_wc.py --raw 1 9 10 0
+bt_hci_cmd_test_wc.py -d --raw 1 9 10 0 
 rem HCI_Read_Local_Name
-bt_hci_cmd_test_wc.py --raw 1 14 c 0
+bt_hci_cmd_test_wc.py -d --raw 1 14 c 0 
 rem HCI_Read_Local_Version_Information
-bt_hci_cmd_test_wc.py --raw 1 1 10 0
+bt_hci_cmd_test_wc.py -d --raw 1 1 10 0 
 rem HCI_Inquiry
-bt_hci_cmd_test_wc.py --raw 1 1 4 5 33 8b 9e 30 0
+bt_hci_cmd_test_wc.py --raw 1 1 4 5 33 8b 9e 30 0 
 rem HCI_Inquiry_Cancel
-bt_hci_cmd_test_wc.py --raw 1 2 4 0
+bt_hci_cmd_test_wc.py -d --raw 1 2 4 0 
