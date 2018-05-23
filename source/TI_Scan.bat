2904cfc91f83d77e5a32a231cf530826bec6ce8b
wayne.chiu <wayne.chiu@wewisetek.com>
2018-05-23 17:24:00 +0800

    add windows script - scan (including initialization for TI)

--- source/TI_Scan.bat
+++
@@ -1,7 +0,0 @@
-rem this is for TI Dongle CC2540
-bt_hci_cmd_test_wc.py --raw 1 0 fe 26 08 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
-bt_hci_cmd_test_wc.py --raw 1 31 fe 1 15
-bt_hci_cmd_test_wc.py --raw 1 31 fe 1 16
-bt_hci_cmd_test_wc.py --raw 1 31 fe 1 1A
-bt_hci_cmd_test_wc.py --raw 1 31 fe 1 19
-bt_hci_cmd_test_wc.py --raw 1 4 fe 3 3 1 0 --delay 10
---
+++ source/script/win/ti_scan.bat
@@ -0,0 +1,6 @@
+rem @echo off
+rem this is for TI Dongle CC2540
+cls
+
+cd data
+ti_init.bat 1
